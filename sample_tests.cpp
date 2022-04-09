9f05def2de887848f08b016711582dfb41bc22b6
Oleksandr Yefremov <ext-oleksandr.yefremov@here.com>
2022-04-09 23:00:46 +0300

    productExceptSelf solution suggested by copilot

--- sample_tests.cpp
+++ sample_tests.cpp
@@ -1,9 +1,21 @@
+#include "header.h"
+
 #include <gtest/gtest.h>
 
-TEST(sample_tests, sample_test) {
-    EXPECT_EQ(1, 1);
+TEST(product, simple) {
+    Solution s;
+    auto res = s.productExceptSelf({1, 2, 3, 4});
+    EXPECT_EQ(res, vector<int>({24, 12, 8, 6}));
 }
 
-TEST(sample_tests, failed_test) {
-    EXPECT_EQ(1, 2);
+TEST(product, simple2) {
+    Solution s;
+    auto res = s.productExceptSelf({1, 2, 3, 4, 5});
+    EXPECT_EQ(res, vector<int>({120, 60, 40, 30, 24}));
+}
+
+TEST(product, simple3) {
+    Solution s;
+    auto res = s.productExceptSelf({1, 2, 3, 4, 5, 6});
+    EXPECT_EQ(res, vector<int>({720, 360, 240, 180, 144, 120}));
 }
